e1ae1918bbf907ad3129d6845705c6ee05a0e57b
Mushahidjutt <mushahidbuttar352@gmail.com>
2025-07-18 18:39:09 +0500

    Upade Code

--- src/assets/Components/ConditionalRendring.jsx
+++ src/assets/Components/ConditionalRendring.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function ConditionalRendring() {
+  const [name, setName] = useState("");
+  const [showMsg, setShowMsg] = useState(false);
 
+  const handleinputchange = (e) => {
+    setName(e.target.value);
+  };
 
+  const handleClick = () => {
+    setShowMsg(true);
+  };
 
   return (
     <div>
       <h1>Conditinal Rendring </h1>
 
+      <p>Enter Your Name </p>
+      <input
+        className="border-2 bg-blue-100"
+        type="text"
+        value={name}
+        onChange={handleinputchange}
+      />
 
-    
+      <button
+        className="ml-2 px-4 py-1 bg-blue-500 text-white rounded"
+        onClick={handleClick}
+      >
+        Show Name{" "}
+      </button>
 
+      {showMsg ? (
+        name.trim() !== "" ? (
+          <p> Wellcome {name}! </p>
+        ) : (
+          <p> Please enter Your Name </p>
+        )
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default ConditionalRendring
+export default ConditionalRendring;
